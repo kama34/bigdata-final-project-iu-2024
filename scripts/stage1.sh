863ba746875ac56b4570daa5e90c6231365c31d1
kama34 <kamishnikov98@gmail.com>
2024-04-19 02:42:50 +0300

    Write scripts to automate the tasks above.

--- scripts/stage1.sh
+++ scripts/stage1.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+echo "Stage1"
+
+pip install gdown
+url="https://drive.google.com/drive/folders/12B_yF8zr9pCPUFUupnR2Z_JUuEuuJ7qd"
+
+gdown --folder $url -O data
+
+rm data/README.txt
+
 
 python3 scripts/csv_preprocess.py
 python3 scripts/build_projectdb.py

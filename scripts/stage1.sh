fdbad114d79f8d8485e75778bc903e0403d800a4
kama34 <kamishnikov98@gmail.com>
2024-04-19 02:40:56 +0300

    Put the *.avsc and *.java files in output/ folder of your repo.

--- scripts/stage1.sh
+++ scripts/stage1.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+python3 scripts/csv_preprocess.py
+python3 scripts/build_projectdb.py
+
+bash scripts/hdfs_loader.sh
+
+mv ./*java /home/team8/project/bigdata-final-project-iu-2024/output/
+mv ./*avsc /home/team8/project/bigdata-final-project-iu-2024/output/

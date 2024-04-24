2fe62a93e6dc7891f769073bb289aba7645ac024
kama34 <kamishnikov98@gmail.com>
2024-04-24 22:36:26 +0300

    Put the *.avsc files from stage 1 to HDFS in a folder like project/warehouse/avsc.

--- scripts/stage2.sh
+++ scripts/stage2.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+            
+WORKING_DIR=/home/team8/project/bigdata-final-project-iu-2024
+
+hdfs dfs -mkdir -p project/warehouse/avsc
+hdfs dfs -put "$WORKING_DIR/output/"*.avsc /user/team8/project/warehouse/avsc
